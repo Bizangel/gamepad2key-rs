602c0c8469f15736287bc6ddf8fa296c20ba4642
Luis De Avila <bizangel10@gmail.com>
2026-03-02 22:30:35 +0000

    Added emitting keys sample code libxdo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate libxdo;
+
+use libxdo::XDo;
+
 fn main() {
-    println!("Hello, world!");
+    let display = String::from(":1");
+    let xdo = XDo::new(Some(&display)).unwrap();
+
+    // xdo.send_keysequence_down("a", 0);
+    // xdo.send_keysequence_up("a", 0);
+    // xdo.enter_text("", 250_000).unwrap();
 }
